cbc750c326f2d5a64ce2b46e30c9532430feae30
rocknron24 <ceradoeron12ict@gmail.com>
2024-11-28 16:30:46 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Student;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    $students = Student::all();
+    return view('dashboard', compact('students'));
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::post('/student/store', [StudentController::class, 'store']) -> name('student.store');
+Route::delete('student/{student}', [StudentController::class, 'destroy']) -> name('student.destroy');
+require __DIR__.'/auth.php';
